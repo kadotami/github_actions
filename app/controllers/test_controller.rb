404ebab39cfa622cd1b6448c3ab020b4dcee0869
kadotami <mondamin.kqfb@gmail.com>
2020-10-15 22:05:20 +0900

    container

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,22 +1,8 @@
 class TestController < ApplicationControler
   def index
-    a = ['a', 'b', 'c']
-    b = [:x,:y]
-    c = a+b
-  end
-
-  def create
-
-  end
-  def edit   
-    return if true
-    a = '1'
-    a
-  end
-
-  def delete
-    test =1
-    a={a:n, b:c}
-    test
+    a = %w[a b]
+    b = [:x, :y]
+    c = a + b
+    c
   end
 end
